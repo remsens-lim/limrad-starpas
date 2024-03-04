72e2f816bfe0425edaf1ed719336bc3993b8009a
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2024-03-04 11:24:01 +0100

    Initialize repo

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import versioneer
+
+setup(
+    name = "starpas",
+    version = versioneer.get_version(),
+    cmdclass= versioneer.get_cmdclass(),
+    author = "Jonas Witthuhn",
+    author_email= "witthuhn@tropos.de",
+    license= "OSI Approved :: GNU General Public License v3 (GPLv3)",
+    packages= ["starpas"],
+    package_dir={"":"src"},
+    package_data={"starpas": ["share/*.json"]},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'starpas = starpas.click:cli',
+        ],
+    },
+    python_requires=">=3.10",
+    install_requires=[
+    ],
+    extras_require={
+        "nbs": [
+            "jupyter",
+            "nbdev",
+            "nbformat",
+        ],
+        "docs": [
+            "sphinx",
+            "myst-parser",
+            "myst-nb",
+        ],
+    },
+)
