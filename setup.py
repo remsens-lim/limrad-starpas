98407151eb8c294f9d6ca749601eb8ca74b13090
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2024-06-07 16:19:33 +0200

    add requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 import versioneer
 
 setup(
-    name = "starpas",
-    version = versioneer.get_version(),
-    cmdclass= versioneer.get_cmdclass(),
-    author = "Jonas Witthuhn",
-    author_email= "witthuhn@tropos.de",
-    license= "OSI Approved :: GNU General Public License v3 (GPLv3)",
-    packages= ["starpas"],
-    package_dir={"":"src"},
+    name="starpas",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Jonas Witthuhn",
+    author_email="witthuhn@tropos.de",
+    license="OSI Approved :: GNU General Public License v3 (GPLv3)",
+    packages=["starpas"],
+    package_dir={"": "src"},
     package_data={"starpas": ["share/*.json"]},
     include_package_data=True,
     entry_points={
@@ -19,6 +19,16 @@ setup(
     },
     python_requires=">=3.10",
     install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "xarray",
+        "netcdf4",
+        "matplotlib",
+        "pip",
+        "jstyleson",
+        "Click",
+        "toolz",
     ],
     extras_require={
         "nbs": [
