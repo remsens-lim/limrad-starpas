e8f313e3dd78d3251c6dd1ad84b33f8699c753f1
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2024-06-10 09:30:38 +0200

    docs/conf.py: configure markdown parsing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+import starpas
 # sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath('../src/'))
 
 # -- Project information -----------------------------------------------------
 
@@ -22,16 +23,45 @@ copyright = '2024, Jonas Witthuhn'
 author = 'Jonas Witthuhn'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = starpas.__version__
 
 
 # -- General configuration ---------------------------------------------------
+source_suffix = {
+    '.rst': 'restructuredtext',
+    # '.md': 'markdown',
+    '.md': 'myst-nb',
+    '.ipynb': 'myst-nb',
+}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    # 'myst_parser',
+    'myst_nb',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.extlinks',
 ]
+
+nb_execution_mode = "off"
+
+extlinks = {
+    "doi": ("https://doi.org/%s", "doi:"),
+}
+
+graphviz_output_format = "svg"
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable', None),
+    'xarray': ('https://xarray.pydata.org/en/stable', None),
+    'numpy': ('https://numpy.org/doc/stable', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,6 +70,26 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+napoleon_type_aliases = {
+    # general terms
+    "sequence": ":term:`sequence`",
+    "iterable": ":term:`iterable`",
+    "callable": ":py:func:`callable`",
+    "dict_like": ":term:`dict-like <mapping>`",
+    "dict-like": ":term:`dict-like <mapping>`",
+    "path-like": ":term:`path-like <path-like object>`",
+    "mapping": ":term:`mapping`",
+    "file-like": ":term:`file-like <file-like object>`",
+    # special terms
+    # "same type as caller": "*same type as caller*",  # does not work, yet
+    # "same type as values": "*same type as values*",  # does not work, yet
+    "timedelta": "~datetime.timedelta",
+    "string": ":class:`string <str>`",
+    # numpy terms
+    "array_like": ":term:`array_like`",
+    "array_like": ":term:`array_like <array_like>`",
+}
 
 
 # -- Options for HTML output -------------------------------------------------
