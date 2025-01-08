ee8ef6e359ece65667c9522ab7c7b51ea0d76e71
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2025-01-08 10:22:25 +0100

    switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,48 +1,12 @@
+"""
+A minimal setup file for compatibility with versioneer.
+
+All the configuration is in pyproject.toml.
+"""
 from setuptools import setup
 import versioneer
 
 setup(
-    name="starpas",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Jonas Witthuhn",
-    author_email="witthuhn@tropos.de",
-    license="OSI Approved :: GNU General Public License v3 (GPLv3)",
-    packages=["starpas"],
-    package_dir={"": "src"},
-    package_data={"starpas": ["share/*.json", "share/*.md"]},
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'starpas = starpas.click:cli',
-        ],
-    },
-    python_requires=">=3.10",
-    install_requires=[
-        "numpy",
-        "scipy",
-        "pandas",
-        "xarray",
-        "netcdf4",
-        "addict",
-        "parse",
-        "matplotlib",
-        "pip",
-        "jstyleson",
-        "Click",
-        "toolz",
-        "imufusion"
-    ],
-    extras_require={
-        "nbs": [
-            "jupyter",
-            "nbdev",
-            "nbformat",
-        ],
-        "docs": [
-            "sphinx",
-            "myst-parser",
-            "myst-nb",
-        ],
-    },
+    # version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
 )
